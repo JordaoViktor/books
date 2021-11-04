e1f934adeed3c1491495716befb7c503899056f2
JordaoViktor <jordaovpinheiro@gmail.com>
2021-11-04 20:30:54 -0300

    :building_construction: Add ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+
 import { Provider } from 'react-redux';
-import { Routes } from './src/routes/index.routes'
 import { store } from '@store/index';
+
+import { ThemeProvider } from 'styled-components';
+
+import { Routes } from './src/routes/index.routes'
+import { theme } from '@styles/index';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </Provider>
   );
 }
